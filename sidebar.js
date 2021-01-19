b9a0587cfd82bdcd8b94f2d3ead04e6ea62ecb48
ASU-VLoND <itmcdono@asu.edu>
2021-01-19 01:00:59 -0700

    Typos fixed and more images/funding information added

--- sidebar.js
+++ sidebar.js
@@ -1,9 +1,9 @@
 function open_sidebar() {
-    document.getElementById("sidebar").style.display = "block";
-    document.getElementById("overlay").style.display = "block";
-  }
-    
-  function close_sidebar() {
-    document.getElementById("sidebar").style.display = "none";
-    document.getElementById("overlay").style.display = "none";
-  }
+  document.getElementById("sidebar").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function close_sidebar() {
+  document.getElementById("sidebar").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
