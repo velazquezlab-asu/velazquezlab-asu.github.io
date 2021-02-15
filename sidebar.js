a970ebbf40e3640619de990c21b5bcd87093c95f
ASU-VLoND <itmcdono@asu.edu>
2021-02-14 17:02:33 -0700

    Student success page added, Ramon's CV updated. New system for generating navigation buttons

--- sidebar.js
+++ sidebar.js
@@ -7,3 +7,32 @@ function close_sidebar() {
   document.getElementById("sidebar").style.display = "none";
   document.getElementById("overlay").style.display = "none";
 }
+
+// Automatically insert the links to the different pages of the site
+const files = ['index.html', 'labdirector.html', 'labmembers.html', 'research.html', 'publications.html', 'funding.html', 'news.html', 'studentsuccess.html', 'outreach.html', 'contact.html'];
+const names = ['Home', 'Lab Director (PI)', 'Lab Members', 'Research', 'Publications', 'Funding', 'News', 'Student Success', 'Outreach', 'Contact Us'];
+
+const highlight = "w3-bar-item w3-button w3-padding w3-hover-text-orange w3-text-orange w3-hover-black w3-black".split(' ');
+const normal = "w3-bar-item w3-button w3-padding w3-hover-text-orange w3-hover-black w3-dark-grey".split(' ');
+
+let file = document.getElementById('page-id').innerText;
+
+for (let i = 0; i < files.length; i++) {
+  let current_file = files[i];
+  let current_name = names[i];
+
+  let a = document.createElement('a');
+
+  if (current_file == file) {
+    a.href = "#";
+    a.onclick = "close_sidebar()";
+    highlight.forEach(item => a.classList.add(item));
+  } else {
+    a.href = "./" + current_file;
+    normal.forEach(item => a.classList.add(item));
+  }
+
+  a.innerText = current_name;
+
+  document.getElementById('sidebar').appendChild(a);
+}
